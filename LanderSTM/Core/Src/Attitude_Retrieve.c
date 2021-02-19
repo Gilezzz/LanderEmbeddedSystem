8f0ed1ef1b60539f5fd86f7cb086584803b77ee6
MarieGalante <21199455+MarieGalante@users.noreply.github.com>
2021-02-19 18:12:13 +0000

    Wrote Initialise_Attitude in Attitude_Retrieve.c

--- LanderSTM/Core/Src/Attitude_Retrieve.c
+++ LanderSTM/Core/Src/Attitude_Retrieve.c
@@ -5,36 +5,46 @@
  *      Author: alexh
  */
 
-
+/* ---------------------------------------------------------------------------*/
 /* Includes ------------------------------------------------------------------*/
+/* ---------------------------------------------------------------------------*/
+
 #include "lsm6dsr.h"
+LSM6DSR_Object_t *pAttitudeObj;  // Create instance of the LPS22HH_Object_t object
+const uint8_t FIFO_Mode = 6;  // LSM6DSR_STREAM_MODE
 
 
+/* ---------------------------------------------------------------------------*/
 /* Functions -----------------------------------------------------------------*/
-
+/* ---------------------------------------------------------------------------*/
 
 /**
  * @brief  Get the LPS22HH temperature sensor output data rate
- * @param  pObj the device pObj
- * @param  Odr pointer where the output data rate is written
+ * @param  pAttitudeObj pointer to the device object
+ * @param  Odr float where the output data rate is written
  * @retval
  */
 void Initialise_Attitude(uint8_t Mode, float Odr)	{
 	/*
 	 * Initialise Sensor
-	 *
+	 * Enable Accelerometer
+	 * Set output data rate
 	 */
 
-	LSM6DSR_Init(LSM6DSR_Object_t *pObj);  // Initialise the IMU
-	LSM6DSR_ACC_Enable(LSM6DSR_Object_t *pObj);  // Enable the accelerometer
-	LSM6DSR_ACC_SetOutputDataRate(LSM6DSR_Object_t *pObj, float Odr);  // Set Odr
-	LSM6DSR_ACC_SetFullScale(LSM6DSR_Object_t *pObj, int32_t FullScale); // Set full scale
-	LSM6DSR_ACC_Enable_Tilt_Detection(LSM6DSR_Object_t *pObj, LSM6DSR_SensorIntPin_t IntPin); // Enable tilt detection
-	LSM6DSR_ACC_Enable_6D_Orientation(LSM6DSR_Object_t *pObj, LSM6DSR_SensorIntPin_t IntPin); // Enable 6D orientation
+	LSM6DSR_Init(pAttitudeObj);  // Initialise the IMU
+	LSM6DSR_ACC_Enable(pAttitudeObj);  // Enable the accelerometer
+	LSM6DSR_ACC_SetOutputDataRate(pAttitudeObj, Odr);  // Set Odr
+//	LSM6DSR_ACC_SetFullScale(LSM6DSR_Object_t *pObj, int32_t FullScale); // Set full scale
+//	LSM6DSR_ACC_Enable_Tilt_Detection(LSM6DSR_Object_t *pObj, LSM6DSR_SensorIntPin_t IntPin); // Enable tilt detection
+//	LSM6DSR_ACC_Enable_6D_Orientation(LSM6DSR_Object_t *pObj, LSM6DSR_SensorIntPin_t IntPin); // Enable 6D orientation
 
-	LSM6DSR_FIFO_Set_Mode(LSM6DSR_Object_t *pObj, uint8_t Mode); // Set FIFO mode
+	LSM6DSR_FIFO_Set_Mode(pAttitudeObj, FIFO_Mode); // Set FIFO mode
 
-	LSM6DSR_ACC_Get_6D_Orientation_XL(LSM6DSR_Object_t *pObj, uint8_t *XLow); // Get 6D orientation
-	LSM6DSR_ACC_GetSensitivity(LSM6DSR_Object_t *pObj, float *Sensitivity); // Get sensitivity
+//	LSM6DSR_ACC_Get_6D_Orientation_XL(pAttitudeObj, uint8_t *XLow); // Get 6D orientation
+//	LSM6DSR_ACC_GetSensitivity(pAttitudeObj, float *Sensitivity); // Get sensitivity
 
+}
+
+float Get_Attitude()	{
+	// TODO
 }
--- LanderSTM/Core/Src/Pressure_Retrieve.c
+++ LanderSTM/Core/Src/Pressure_Retrieve.c
@@ -5,23 +5,28 @@
  *      Author: alexh
  */
 
+/* ---------------------------------------------------------------------------*/
 /* Includes ------------------------------------------------------------------*/
+/* ---------------------------------------------------------------------------*/
+
 #include "lps22hh.h"
 LPS22HH_Object_t *pPressObj;  // Create instance of the LPS22HH_Object_t object
-const uint8_t Mode = 6;
+const uint8_t Mode = 2;  // LPS22HH_STREAM_MODE
 
-// Decalre global ish ground pressure value
+// Decalre global ish ground pressure value TODO
 
 
-
+/* ---------------------------------------------------------------------------*/
 /* Functions ------------------------------------------------------------------*/
+/* ---------------------------------------------------------------------------*/
 
 /**
  * @brief  Get the LPS22HH temperature sensor output data rate
- * @param  Odr pointer where the output data rate is written
+ * @param  Odr the output data rate value to be set
+ * @param  Pressure_Ground_loc pointer where the ground pressure is stored
  * @retval Nothing, yet...
  */
-void Initialise_Press(float Odr)	{
+void Initialise_Press(float Odr, float *Pressure_Ground_loc)	{
 	/*
 	 * Enable drdy
 	 * Fifo Set mode 1
@@ -41,21 +46,25 @@ void Initialise_Press(float Odr)	{
 	LPS22HH_Enable_DRDY_Interrupt(pPressObj);  // Enable the DRDY Interrupt
 	LPS22HH_FIFO_Set_Mode(pPressObj, Mode);  // Set the FIFO mode
 	LPS22HH_PRESS_SetOutputDataRate(pPressObj, Odr);  // Set the output data rate
-	// RECORD ground value of pressure. TODO
+
+	LPS22HH_PRESS_GetPressure(pPressObj, Pressure_Ground_loc); // RECORD ground value of pressure. TODO
 }
 
 
 /**
- * @brief  TO DO
- * @param  Value pointer where the output data rate is written
+ * @brief  Get the pressure at the current location, do some maths using the ground pressure to find the current altitude
+ * @param  Pressure_loc pointer where the pressure value is written
+ * @param  Pressure_Ground_loc pointer where the ground pressure is stored
  * @retval Nothing, yet...
  */
-void Get_Altitude(float *Value_loc)	{
+void Get_Altitude(float *Pressure_loc, float *Pressure_Ground_loc)	{
 	/*
-	* Get press
-	* Convert to altitude TODO
+	* Get pressure from sensor
+	* Convert to altitude
 	*/
-	LPS22HH_PRESS_GetPressure(pPressObj, Value_loc);
+	LPS22HH_PRESS_GetPressure(pPressObj, Pressure_loc);
+
+	return (*Pressure_Ground_loc - *Pressure_loc)/1.225/9.81;  // Convert the pressure to an altitude TODO: use temp to calculate density
 }
 
 
