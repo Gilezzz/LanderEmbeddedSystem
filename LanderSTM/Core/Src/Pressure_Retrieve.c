2fabf4d4924d63330dde396849fbc8afe17b2717
MarieGalante <21199455+MarieGalante@users.noreply.github.com>
2021-02-19 16:11:58 +0000

    Made grammatical changes.

--- LanderSTM/Core/Src/Pressure_Retrieve.c
+++ LanderSTM/Core/Src/Pressure_Retrieve.c
@@ -7,20 +7,21 @@
 
 /* Includes ------------------------------------------------------------------*/
 #include "lps22hh.h"
-
 LPS22HH_Object_t *pPressObj;  // Create instance of the LPS22HH_Object_t object
+const uint8_t Mode = 6;
+
+// Decalre global ish ground pressure value
+
 
 
 /* Functions ------------------------------------------------------------------*/
 
-
 /**
  * @brief  Get the LPS22HH temperature sensor output data rate
- * @param  Mode FIFO operating mode
  * @param  Odr pointer where the output data rate is written
  * @retval Nothing, yet...
  */
-void Initialise_Press(uint8_t Mode, float Odr)	{
+void Initialise_Press(float Odr)	{
 	/*
 	 * Enable drdy
 	 * Fifo Set mode 1
@@ -34,14 +35,27 @@ void Initialise_Press(uint8_t Mode, float Odr)	{
 	//uint8_t Mode = 1;  // Mode for the FIFO
 	//float Odr = 9600;  // The output data rate
 
-//	LPS22HH_Initialize(LPS22HH_Object_t *pObj); // is this needed?
-	LPS22HH_Init(*pPressObj);  // Initialise the sensor
-	LPS22HH_PRESS_Enable(*pPressObj);  // Enable the pressure sensor
-	LPS22HH_TEMP_Disable(*pPressObj);  // Disable the temperature sensor
-	LPS22HH_Enable_DRDY_Interrupt(*pPressObj);  // Enable the DRDY Interrupt
-	LPS22HH_FIFO_Set_Mode(*pPressObj, Mode);  // Set the FIFO mode
-	LPS22HH_PRESS_SetOutputDataRate(*pPressObj, Odr);  // Set the output data rate
+	LPS22HH_Init(pPressObj);  // Initialise the sensor
+	LPS22HH_PRESS_Enable(pPressObj);  // Enable the pressure sensor
+	LPS22HH_TEMP_Disable(pPressObj);  // Disable the temperature sensor
+	LPS22HH_Enable_DRDY_Interrupt(pPressObj);  // Enable the DRDY Interrupt
+	LPS22HH_FIFO_Set_Mode(pPressObj, Mode);  // Set the FIFO mode
+	LPS22HH_PRESS_SetOutputDataRate(pPressObj, Odr);  // Set the output data rate
+	// RECORD ground value of pressure. TODO
+}
 
+
+/**
+ * @brief  TO DO
+ * @param  Value pointer where the output data rate is written
+ * @retval Nothing, yet...
+ */
+void Get_Altitude(float *Value_loc)	{
+	/*
+	* Get press
+	* Convert to altitude TODO
+	*/
+	LPS22HH_PRESS_GetPressure(pPressObj, Value_loc);
 }
 
 
