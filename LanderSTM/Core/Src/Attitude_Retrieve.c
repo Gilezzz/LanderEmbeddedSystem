18d4901cf8b0ac425d109416fc9552329b1f46db
MarieGalante <21199455+MarieGalante@users.noreply.github.com>
2021-02-21 12:49:42 +0000

    Get acceleration functions and tidying up documentation.

--- LanderSTM/Core/Src/Attitude_Retrieve.c
+++ LanderSTM/Core/Src/Attitude_Retrieve.c
@@ -10,8 +10,9 @@
 /* ---------------------------------------------------------------------------*/
 
 #include "lsm6dsr.h"
-LSM6DSR_Object_t *pAttitudeObj;  // Create instance of the LPS22HH_Object_t object
+LSM6DSR_Object_t *pAttitudeObj;  // Create instance of the LSM6DSR_Object_t object TODO
 const uint8_t FIFO_Mode = 6;  // LSM6DSR_STREAM_MODE
+
 
 
 /* ---------------------------------------------------------------------------*/
@@ -19,32 +20,76 @@ const uint8_t FIFO_Mode = 6;  // LSM6DSR_STREAM_MODE
 /* ---------------------------------------------------------------------------*/
 
 /**
- * @brief  Get the LPS22HH temperature sensor output data rate
- * @param  pAttitudeObj pointer to the device object
- * @param  Odr float where the output data rate is written
- * @retval
+ * @brief  Initialise the LSM6DSR accelerometer by calling the init, enable and Odr functions.
+ * @param  Odr float the output data rate to be set
+ * @retval Nothing
  */
-void Initialise_Attitude(uint8_t Mode, float Odr)	{
-	/*
-	 * Initialise Sensor
-	 * Enable Accelerometer
-	 * Set output data rate
-	 */
-
+void Initialise_Attitude(float Odr)	{
+	// TODO, check that we are looking at the right axis for the acceleration orientation
+	/* Init Sensor*/
 	LSM6DSR_Init(pAttitudeObj);  // Initialise the IMU
-	LSM6DSR_ACC_Enable(pAttitudeObj);  // Enable the accelerometer
-	LSM6DSR_ACC_SetOutputDataRate(pAttitudeObj, Odr);  // Set Odr
-//	LSM6DSR_ACC_SetFullScale(LSM6DSR_Object_t *pObj, int32_t FullScale); // Set full scale
-//	LSM6DSR_ACC_Enable_Tilt_Detection(LSM6DSR_Object_t *pObj, LSM6DSR_SensorIntPin_t IntPin); // Enable tilt detection
-//	LSM6DSR_ACC_Enable_6D_Orientation(LSM6DSR_Object_t *pObj, LSM6DSR_SensorIntPin_t IntPin); // Enable 6D orientation
-
 	LSM6DSR_FIFO_Set_Mode(pAttitudeObj, FIFO_Mode); // Set FIFO mode
 
-//	LSM6DSR_ACC_Get_6D_Orientation_XL(pAttitudeObj, uint8_t *XLow); // Get 6D orientation
-//	LSM6DSR_ACC_GetSensitivity(pAttitudeObj, float *Sensitivity); // Get sensitivity
+	/* Accelerometer */
+	LSM6DSR_ACC_Enable(pAttitudeObj);  // Enable the accelerometer
+	LSM6DSR_ACC_SetOutputDataRate(pAttitudeObj, Odr);  // Set Odr
 
+	/* Gyro */
+	LSM6DSR_GYRO_Enable(pAttitudeObj);  // Enable the GYRO so we can get acceleration rates
+	LSM6DSR_GYRO_SetOutputDataRate(pAttitudeObj, Odr);  // Set Odr
+
+	/* Possibly usefull functions*/
+	// LSM6DSR_FIFO_ACC_Set_BDR(LSM6DSR_Object_t *pObj, float Bdr) TODO
+	//	LSM6DSR_ACC_SetFullScale(LSM6DSR_Object_t *pObj, int32_t FullScale); // Set full scale
+	//	LSM6DSR_ACC_Enable_Tilt_Detection(LSM6DSR_Object_t *pObj, LSM6DSR_SensorIntPin_t IntPin); // Enable tilt detection
+	//	LSM6DSR_ACC_Enable_6D_Orientation(LSM6DSR_Object_t *pObj, LSM6DSR_SensorIntPin_t IntPin); // Enable 6D orientation
+	//	LSM6DSR_ACC_Get_6D_Orientation_XL(pAttitudeObj, uint8_t *XLow); // Get 6D orientation
+	//	LSM6DSR_ACC_GetSensitivity(pAttitudeObj, float *Sensitivity); // Get sensitivity
 }
 
-float Get_Attitude()	{
-	// TODO
+/**
+ * @brief  Measure the current acceleration and angular rate values and write to location
+ * @param  pAcceleration pointer to a LSM6DSR_Axes_t struct containing the acceleration in the three axes
+ * @param  pAngularRate pointer to a LSM6DSR_Axes_t struct containing the angular rates
+ * @retval Nothing
+ */
+void Update_ACC_GYRO(LSM6DSR_Axes_t *pAcceleration, LSM6DSR_Axes_t *pAngularRate)	{
+	/* Accelerometer */
+	LSM6DSR_ACC_GetAxes(pAttitudeObj, pAcceleration);
+	// LSM6DSR_ACC_GetAxesRaw(LSM6DSR_Object_t *pObj, LSM6DSR_AxesRaw_t *Value)	
+	
+	/* Gyro */
+	LSM6DSR_GYRO_GetAxes(pAttitudeObj, pAngularRate);
+	// LSM6DSR_GYRO_GetAxesRaw(LSM6DSR_Object_t *pObj, LSM6DSR_AxesRaw_t *Value)
 }
+
+
+/**
+ * @brief  Read the x acceleration stored at the pAcceleration struct
+ * @param  pAcceleration pointer to a LSM6DSR_Axes_t struct containing the acceleration in the three axes
+ * @retval x the acceleration in the x direction
+ */
+int32_t Read_ACC_x(LSM6DSR_Axes_t *pAcceleration)	{
+	return pAcceleration->x;
+}
+
+
+/**
+ * @brief  Read the y acceleration stored at the pAcceleration struct
+ * @param  pAcceleration pointer to a LSM6DSR_Axes_t struct containing the acceleration in the three axes
+ * @retval x the acceleration in the x direction
+ */
+int32_t Read_ACC_y(LSM6DSR_Axes_t *pAcceleration)	{
+	return pAcceleration->y;
+}
+
+
+/**
+ * @brief  Read the z acceleration stored at the pAcceleration struct
+ * @param  pAcceleration pointer to a LSM6DSR_Axes_t struct containing the acceleration in the three axes
+ * @retval x the acceleration in the x direction
+ */
+int32_t Read_ACC_z(LSM6DSR_Axes_t *pAcceleration)	{
+	return pAcceleration->z;
+}
+
--- LanderSTM/Core/Src/Pressure_Retrieve.c
+++ LanderSTM/Core/Src/Pressure_Retrieve.c
@@ -10,61 +10,57 @@
 /* ---------------------------------------------------------------------------*/
 
 #include "lps22hh.h"
-LPS22HH_Object_t *pPressObj;  // Create instance of the LPS22HH_Object_t object
-const uint8_t Mode = 2;  // LPS22HH_STREAM_MODE
+LPS22HH_Object_t *pPressObj;  // Create instance of the LPS22HH_Object_t object TODO
+const uint8_t FIFO_Mode = 2;  // LPS22HH_STREAM_MODE
+float *pPressure_Ground_loc;  // Decalre ground pressure value
 
-// Decalre global ish ground pressure value TODO
 
 
 /* ---------------------------------------------------------------------------*/
-/* Functions ------------------------------------------------------------------*/
+/* Functions -----------------------------------------------------------------*/
 /* ---------------------------------------------------------------------------*/
 
 /**
- * @brief  Get the LPS22HH temperature sensor output data rate
+ * @brief  Initialise the LPS22HH device by setting DRDY, FIFO mode, obtaining ground pressure, pressure enable, and Odr
  * @param  Odr the output data rate value to be set
- * @param  Pressure_Ground_loc pointer where the ground pressure is stored
+ * @param  pPressure_Ground_loc pointer where the ground pressure is stored
  * @retval Nothing, yet...
  */
-void Initialise_Press(float Odr, float *Pressure_Ground_loc)	{
+void Initialise_Press(float Odr, float *pPressure_Ground_loc)	{
 	/*
+	 * LPS22H_init
 	 * Enable drdy
 	 * Fifo Set mode 1
 	 * Get_press
 	 * 		LPS22HH_PRESS_GetPressure(LPS22HH_Object_t *pObj, float *Value)
-	 * LPS22H_init
 	 * LPS22H_temp_disable
 	 * LPS22H_press_enable
 	 * LPS22H_press_set_output_dataRate
 	 */
-	//uint8_t Mode = 1;  // Mode for the FIFO
-	//float Odr = 9600;  // The output data rate
 
 	LPS22HH_Init(pPressObj);  // Initialise the sensor
 	LPS22HH_PRESS_Enable(pPressObj);  // Enable the pressure sensor
 	LPS22HH_TEMP_Disable(pPressObj);  // Disable the temperature sensor
 	LPS22HH_Enable_DRDY_Interrupt(pPressObj);  // Enable the DRDY Interrupt
-	LPS22HH_FIFO_Set_Mode(pPressObj, Mode);  // Set the FIFO mode
+	LPS22HH_FIFO_Set_Mode(pPressObj, FIFO_Mode);  // Set the FIFO mode
 	LPS22HH_PRESS_SetOutputDataRate(pPressObj, Odr);  // Set the output data rate
-
-	LPS22HH_PRESS_GetPressure(pPressObj, Pressure_Ground_loc); // RECORD ground value of pressure. TODO
+	LPS22HH_PRESS_GetPressure(pPressObj, pPressure_Ground_loc); // RECORD ground value of pressure.
 }
 
 
 /**
  * @brief  Get the pressure at the current location, do some maths using the ground pressure to find the current altitude
- * @param  Pressure_loc pointer where the pressure value is written
- * @param  Pressure_Ground_loc pointer where the ground pressure is stored
+ * @param  pPressure_loc pointer where the current pressure value will be written
+ * @param  pPressure_Ground_loc pointer where the ground pressure is stored. This was done during the Initialise_Press
  * @retval Nothing, yet...
  */
-void Get_Altitude(float *Pressure_loc, float *Pressure_Ground_loc)	{
-	/*
-	* Get pressure from sensor
-	* Convert to altitude
-	*/
-	LPS22HH_PRESS_GetPressure(pPressObj, Pressure_loc);
+void Get_Altitude(float *pPressure_loc, float *pPressure_Ground_loc)	{
 
-	return (*Pressure_Ground_loc - *Pressure_loc)/1.225/9.81;  // Convert the pressure to an altitude TODO: use temp to calculate density
+	/* Write the pressure at the current time to pPressure_loc */
+	LPS22HH_PRESS_GetPressure(pPressObj, pPressure_loc);
+
+	/* By using the value of pressure recorded during the initialise function we can calulate the current altutude */
+	return (*pPressure_Ground_loc - *pPressure_loc)/1.225/9.81;  // TODO: use temp to calculate density
 }
 
 
